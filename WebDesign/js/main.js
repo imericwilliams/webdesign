ad6cd7c1682bfadc5c7be86f17eec2b0b06b6e91
Eric Williams <eric10718@gmail.com>
2018-09-25 18:39:44 -0400

    eric

---
+++ WebDesign/js/main.js
@@ -0,0 +1,20 @@
+// jquery file for main page
+
+$(document).ready(function() {
+    // run function on initial page load
+    // functionName();
+
+    // run function on resize of the window
+    $(window).resize(function() {
+      
+    });
+    // run function on scroll
+    $(window).scroll(function() {
+
+    });
+});
+
+
+function functionName() {
+  // your function goes here
+}
