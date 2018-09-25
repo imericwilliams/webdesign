b25c6aed0791b385a5bcc99cb8838e531771ccc9
Eric Williams <eric10718@gmail.com>
2018-09-25 16:35:35 -0400

    eric

--- singleServeweb/script.js
+++ singleServeweb/script.js
@@ -1,8 +1,5 @@
-var images = ["morocco.jpg"]
-
-
-function newCountry() {
-    var random = Math.floor(Math.random() * (images.length));
-
-    document.getElementById("morocco").innerHTML = images[random];
-    }
+var images = ("images/australia2.jpg", "images/france.jpg", "images/greece.jpg", "images/india2.jpg", "images/ireland.jpg", "images/italy2.jpg", "images/japan.jpg", "images/mexico.jpg", "images/morocco.jpg", "images/netherlands.jpg", "images/southAfrica.jpg", "images/spain.jpg","images/sriLanka.jpg", "images/thailand.jpg", "images/turkey.jpg", "images/unitedStates.jpg")
+  function choosePic() {
+      var random = Math.floor(Math.random() * (images.length));
+      document.getElementById("countries").innerHTML = images[random];
+      }
