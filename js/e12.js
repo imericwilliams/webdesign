d36cc3585f78fbf5f8423853b7ed2c663e2710cf
Eric Williams <eric10718@gmail.com>
2018-11-15 01:45:14 -0500

    eric

---
+++ js/e12.js
@@ -0,0 +1,14 @@
+//$(document).ready(function() {
+    //interactions();
+
+//function interactions() {
+//  $('.jqHover').click(function(){
+  //    $('.number4').toggleClass('moveITR');
+  //    console.log('hovered');
+//}
+
+
+var divdbl = $( "div:first" );
+divdbl.dblclick(function() {
+  divdbl.toggleClass( "dbl" );
+});
